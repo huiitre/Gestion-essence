fbb536b5aa1a1926d1ab5173c1471fcf7606ef5e
huiitre <yanis.deluca@laposte.net>
2022-08-15 21:51:21 +0200

    init du projet / ajout de sass/sass-loader / ajout de vue-router / début de mise en place du module de connexion à l'API déjà existante

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
