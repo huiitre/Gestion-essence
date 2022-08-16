9ff8351c893510ccd4e2e50b0a4d5747fd9de5c6
huiitre <yanis.deluca@laposte.net>
2022-08-16 23:42:46 +0200

    tentative de mise en place de toastification mais pas le temps, rollback vers mosha toast

--- src/modules/Login/LoginHooks.js
+++ src/modules/Login/LoginHooks.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// import { clearToasts  } from 'mosha-vue-toastify'
-// import { errorToast, spinnerToast, successToast } from '@/modules/common/components/toasts.js'
+import { clearToasts  } from 'mosha-vue-toastify'
+import { errorToast, spinnerToast, successToast } from '@/modules/common/components/toasts.js'
 
 const client = axios.create({
   baseURL: process.env.VUE_APP_ROOT_API,
@@ -12,19 +12,19 @@ const client = axios.create({
 
 export default {
   login: async (credentials) => {
-    // spinnerToast('Connexion en cours ...');
+    spinnerToast('Connexion en cours ...');
     return await client.post('/login_check', credentials)
       .then ((res) => {
         console.log('res : ', res.data);
-        // successToast('Vous êtes connecté !');
+        successToast('Vous êtes connecté !');
 
       })
       .catch((e) => {
         console.log('error : ', e.response.data.message);
-        // errorToast(e.response.data.message);
+        errorToast(e.response.data.message);
       })
       .finally(() => {
-        // clearToasts();
+        clearToasts();
       });
   }
 }
--- src/modules/common/components/toasts.js
+++ src/modules/common/components/toasts.js
@@ -1,55 +1,48 @@
-// import { createToast } from 'mosha-vue-toastify';
-// import 'mosha-vue-toastify/dist/style.css';
+import { createToast } from 'mosha-vue-toastify';
+import 'mosha-vue-toastify/dist/style.css';
 
-
-
-export const successToast = (message) => {
-	/* createToast(message, {
+export const successToast = (message) => {createToast(message, {
 		hideProgressBar: 'false',
 		showIcon: 'true',
 		position: 'top-center',
 		type: 'success',
 		transition: 'bounce',
     swipeClose: true
-	}); */
+	});
 };
-export const errorToast = (message) => {
-	/* createToast(message, {
+export const errorToast = (message) => {createToast(message, {
 		hideProgressBar: 'false',
 		showIcon: 'true',
 		position: 'top-center',
 		type: 'danger',
 		transition: 'bounce',
     swipeClose: true
-	}); */
+	});
 };
-export const warnToast = (message) => {
-	/* createToast(message, {
+export const warnToast = (message) => {createToast(message, {
 		hideProgressBar: 'false',
 		showIcon: 'true',
 		position: 'top-center',
 		type: 'warning',
 		transition: 'bounce',
     swipeClose: true
-	}); */
+	});
 };
-export const infoToast = (message) => {
-	/* createToast(message, {
+export const infoToast = (message) => {createToast(message, {
 		hideProgressBar: 'false',
 		showIcon: 'true',
 		position: 'top-center',
 		type: 'info',
 		transition: 'bounce',
     swipeClose: true
-	}); */
+	});
 };
-export const spinnerToast = (message) => {
-	/* createToast(message, {
+export const spinnerToast = (message) => {createToast(message, {
 		hideProgressBar: 'false',
 		showIcon: 'true',
 		position: 'top-center',
 		type: 'info',
 		transition: 'bounce',
     timeout: 'none'
-	}); */
+	});
 };
