2035731820a731adcf4c2a9f193449d296a09e37
huiitre <yanis.deluca@laposte.net>
2022-08-28 17:54:03 +0200

    test build cordova

--- src/modules/Home/components/Home.vue
+++ src/modules/Home/components/Home.vue
@@ -4,10 +4,6 @@
       <div class="module-item">Gestion Essence</div>
       <div class="module-item">Gestion Essence</div>
       <div class="module-item">Gestion Essence</div>
-      <br /><br />
-      <router-link class="link-test" to="/">home</router-link><br>
-      <router-link class="link-test" to="/home2">home2</router-link><br>
-      <router-link class="link-test" to="/home3">home3</router-link><br>
     </div>
   </main>
 </template>
@@ -23,8 +19,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.link-test, .test {
-  margin-top: 6rem;
-  color: #fff;
-}
+
 </style>
